352229fb0cf889062bbb70adc1900fcf0b69d473
vruban <vova.ruban9@gmail.com>
2020-08-12 12:45:45 +0300

    refact

--- src/components/BodyDropDown.jsx
+++ src/components/BodyDropDown.jsx
@@ -1,42 +1,60 @@
 import React, { useState } from "react";
 
-import classnames from "classnames";
-
 import styles from "./BodyDropDown.module.css";
 
 export const BodyDropDownOne = () => {
-  const [showOne, setShowOne] = useState(false);
-  const [showTwo, setShowTwo] = useState(false);
+  const [active, setActive] = useState(0);
 
-  const onClickOne = () => {
-    setShowOne(!showOne);
+  const onClickOne = (value) => {
+    setActive(value);
+    console.log(value);
   };
-  const onClickTwo = () => {
-    setShowTwo(!showTwo);
+
+  const podMenu = (active) => {
+    switch (active) {
+      case 1:
+        return (
+          <div>
+            <div>2.1.1</div>
+            <div>2.1.2</div>
+            <div>2.1.3</div>
+          </div>
+        );
+      case 2:
+        return (
+          <div>
+            <div>2.2.1</div>
+            <div>2.2.2</div>
+            <div>2.2.3</div>
+          </div>
+        );
+      case 3:
+        return (
+          <div>
+            <div>2.3.1</div>
+            <div>2.3.2</div>
+            <div>2.3.3</div>
+          </div>
+        );
+      default:
+        return "";
+    }
   };
+
   return (
     <div id="wrap" className={styles.wrapper}>
       <div className={styles.elems}>
-        <div>Элем1</div>
         <div className={styles.elem}>
-          <div onClick={onClickOne}> Элем2</div>
-          {showOne && (
-            <div>
-              <div>2.2.1</div>
-              <div>2.2.2</div>
-              <div>2.2.3</div>
-            </div>
-          )}
+          <div onClick={() => onClickOne(1)}>Элем1</div>
+          {active === 1 ? podMenu(active) : ""}
         </div>
         <div className={styles.elem}>
-          <div onClick={onClickTwo}> Элем3</div>
-          {showTwo && (
-            <div>
-              <div>2.3.1</div>
-              <div>2.3.2</div>
-              <div>2.3.3</div>
-            </div>
-          )}
+          <div onClick={() => onClickOne(2)}> Элем2</div>
+          {active === 2 ? podMenu(active) : ""}
+        </div>
+        <div className={styles.elem}>
+          <div onClick={() => onClickOne(3)}> Элем3</div>
+          {active === 3 ? podMenu(active) : ""}
         </div>
         <div>Элем4</div>
       </div>
@@ -45,38 +63,58 @@ export const BodyDropDownOne = () => {
 };
 
 export const BodyDropDownTwo = () => {
-  const [showOne, setShowOne] = useState(false);
-  const [showTwo, setShowTwo] = useState(false);
+  const [active, setActive] = useState(0);
 
-  const onClickOne = () => {
-    setShowOne(!showOne);
+  const onClickOne = (value) => {
+    setActive(value);
+    console.log(value);
   };
-  const onClickTwo = () => {
-    setShowTwo(!showTwo);
+
+  const podMenu = (active) => {
+    switch (active) {
+      case 1:
+        return (
+          <div>
+            <div>3.1.1</div>
+            <div>3.1.2</div>
+            <div>3.1.3</div>
+          </div>
+        );
+      case 2:
+        return (
+          <div>
+            <div>3.2.1</div>
+            <div>3.2.2</div>
+            <div>3.2.3</div>
+          </div>
+        );
+      case 3:
+        return (
+          <div>
+            <div>3.3.1</div>
+            <div>3.3.2</div>
+            <div>3.3.3</div>
+          </div>
+        );
+      default:
+        return "";
+    }
   };
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.elems}>
-        <div>Элем3.1</div>
         <div className={styles.elem}>
-          <div onClick={onClickOne}>Элем3.2</div>
-          {showOne && (
-            <div>
-              <div>3.2.1</div>
-              <div>3.2.2</div>
-              <div>3.2.3</div>
-            </div>
-          )}
+          <div onClick={() => onClickOne(1)}>Элем3.1</div>
+          {active === 1 ? podMenu(active) : ""}
         </div>
         <div className={styles.elem}>
-          <div onClick={onClickTwo}> Элем3.4</div>
-          {showTwo && (
-            <div>
-              <div>3.4.1</div>
-              <div>3.4.2</div>
-              <div>3.4.3</div>
-            </div>
-          )}
+          <div onClick={() => onClickOne(2)}>Элем3.2</div>
+          {active === 2 ? podMenu(active) : ""}
+        </div>
+        <div className={styles.elem}>
+          <div onClick={() => onClickOne(3)}> Элем3.3</div>
+          {active === 3 ? podMenu(active) : ""}
         </div>
       </div>
     </div>
